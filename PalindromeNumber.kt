806ef4a15265fd0ef3b4ef0de6a3177762530b64
Cuong Phan <vcuong0909@gmail.com>
2021-08-25 16:09:29 -0400

    Update and Completed All Tasks

--- PalindromeNumber.kt
+++ PalindromeNumber.kt
@@ -1,25 +1,27 @@
 package Assignment1
-
+import java.util.Scanner
 /*
 3) Write a program to input a number and check whether it is Palindrome number or not.
  Number is said to be Palindrome number if number and its reverse of the number is same.
  Example: 1771 is Palindrome number because its reverse number is 1771 itself.
  */
-import java.lang.System.exit
-import java.util.Scanner
-import kotlin.system.exitProcess
-
 fun main() {
     val scanner = Scanner(System.`in`)
     print("Enter a number: ")
     val number = scanner.nextInt()
-    val str = number.toString()
+    var temp = number
+    var reverse = 0
 
-    for (i in 0 until str.length / 2) {
-        if (str[i] != str[str.length - 1 -i]) {
-            println("$number is not a Palindrome")
-            exitProcess(0)
-        }
+    while (temp != 0)
+    {
+        val d = temp % 10
+        reverse = reverse * 10 + d
+        temp /= 10
     }
-    println("$number is a Palindrome")
+
+    if (reverse == number) {
+        println("$number is a palindrome")
+    } else {
+        println("$number is not a palindrome")
+    }
 }
